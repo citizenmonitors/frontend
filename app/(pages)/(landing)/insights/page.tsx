d0cb8aef8eddfac1cafc21791d82452f244a8d56
Charles <okoyecharles509@gmail.com>
2025-06-18 07:33:12 +0100

    fix: add dynamic resolution to the press page

--- app/(pages)/(landing)/insights/page.tsx
+++ app/(pages)/(landing)/insights/page.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import insights from "@/app/data/insights/index";
 import InsightCard from "@/app/components/landing/insights/InsightCard";
+import { Insight } from "@/app/data/insights/types";
 
 export default function Insights() {
+  const [currentInsights, setCurrentInsights] = useState<Array<Insight>>([]);
+
+  // Initialize the insights state with the imported insights
+  // This will run once when the component mounts
+  useEffect(() => {
+    setCurrentInsights(insights);
+  }, []);
+
   return (
     <div className="container">
       <header className="text-display-sm lg:text-display-lg font-league text-center leading-tight tracking-tight font-bold mb-5 mt-3 lg:mt-[55px] lg:mb-10 lg:text-left">
@@ -13,7 +23,7 @@ export default function Insights() {
       </header>
 
       <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 xl:gap-24 xxl:gap-32 mb-16 lg:mb-32">
-        {insights.map((insight) => (
+        {currentInsights.map((insight) => (
           <InsightCard key={insight.id} insight={insight} />
         ))}
       </div>
--- app/(pages)/(landing)/press/page.tsx
+++ app/(pages)/(landing)/press/page.tsx
@@ -1,21 +1,49 @@
+"use client";
 import PressCard from "@/app/components/landing/press/PressCard";
 import PressDonateCTA from "@/app/components/landing/press/PressDonateCTA";
 import PressHighlight from "@/app/components/landing/press/PressHighlight";
 import PressSocialCTA from "@/app/components/landing/press/PressSocialCTA";
-import press from "@/app/data/press";
-import React from "react";
+import pressData from "@/app/data/press";
+import { Press as PressType } from "@/app/data/press/types";
+import React, { useEffect, useState } from "react";
 
 export default function Press() {
-  const highlightedArticle = press[0];
-  const articles = press.slice(1, 4);
-  const otherArticles = press.slice(4);
+  type PressState = {
+    highlightedArticle: PressType;
+    articles: Array<PressType>;
+    otherArticles: Array<PressType>;
+  };
+  const [{ highlightedArticle, articles, otherArticles }, setPress] =
+    useState<PressState>({
+      highlightedArticle: {} as PressType,
+      articles: [],
+      otherArticles: [],
+    });
+
+  // This isn't strictly necessary since we initialize the state with the data,
+  useEffect(() => {
+    const currentPressData = pressData.filter(
+      (p) => new Date(p.date) <= new Date()
+    );
+
+    const highlightedArticle = currentPressData[0];
+    const articles = currentPressData.slice(1, 4);
+    const otherArticles = currentPressData.slice(4);
+
+    setPress({
+      highlightedArticle,
+      articles,
+      otherArticles,
+    });
+  }, []);
 
   return (
     <div className="container">
       <header className="text-display-sm lg:text-display-lg font-league text-center leading-tight tracking-tight font-bold mb-5 mt-3 lg:mt-[55px] lg:mb-10 lg:text-left">
         <h1 className="text-brand-500">CITIZEN MONITORS PRESS CENTER:</h1>{" "}
         <h2 className="leading-[0.94] text-gray-700">
-          Your hub for official statements, press releases, and public information.
+          Your hub for official statements, press releases, and public
+          information.
         </h2>
       </header>
 
@@ -36,7 +64,6 @@ export default function Press() {
       </div>
 
       <PressDonateCTA />
-
       <section
         id="other-articles"
         className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-8 lg:gap-9 my-10"
@@ -45,7 +72,6 @@ export default function Press() {
           <PressCard key={index} article={article} />
         ))}
       </section>
-
       <PressSocialCTA />
     </div>
   );
