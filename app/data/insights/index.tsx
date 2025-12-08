2ae9ed2e37e42cf72e6cad895c60205806288e2b
Yunus Mustapha <yunusmustapha2@gmail.com>
2025-12-08 12:59:34 +0100

    Article moved from press to insight

--- app/data/insights/index.tsx
+++ app/data/insights/index.tsx
@@ -4,9 +4,11 @@ import Article3 from './articles/nigeria-elections-01';
 import Article4 from './articles/truth-of-data-01';
 import Article5 from './articles/legitimacy-01';
 import Article6 from './articles/elections-and-governance-01';
+import Article7 from './articles/diplomacy-01';
 import { Insight } from './types';
 
 const insights: Array<Insight> = [
+  Article7,
   Article1,
   Article2,
   Article3,
